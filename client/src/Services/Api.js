c5b4440b0d9cdef80ad4efd07c87aa7ece4c69d3
Dinesh Kumar <dineshsudhakaran2706@gmail.com>
2026-03-17 15:34:50 +0530

    backend update

--- client/src/Services/Api.js
+++ client/src/Services/Api.js
@@ -1,53 +1,8 @@
-let workflows = [
-  {
-    _id: "1",
-    name: "Leave Approval",
-    type: "HR",
-    version: 1,
-    status: "Pending",
-    startedBy: "Dinesh",
-    startDate: "17-03-2026",
-    endDate: "-",
-    is_active: true
-  },
-  {
-    _id: "2",
-    name: "Salary Processing",
-    type: "Finance",
-    version: 1,
-    status: "Approved",
-    startedBy: "Admin",
-    startDate: "15-03-2026",
-    endDate: "16-03-2026",
-    is_active: true
-  }
-]
+import axios from "axios"
 
-export const getWorkflows = async () => {
-  return { data: workflows }
-}
+const API = "http://localhost:5173/api/workflows"
 
-export const createWorkflow = async (data) => {
-  const newWorkflow = {
-    _id: Date.now().toString(),
-    ...data,
-    version: 1,
-    status: "Pending",
-    startedBy: "You",
-    startDate: new Date().toLocaleDateString(),
-    endDate: "-",
-    is_active: true
-  }
-  workflows.push(newWorkflow)
-  return { data: newWorkflow }
-}
-
-export const deleteWorkflow = async (id) => {
-  workflows = workflows.filter(w => w._id !== id)
-}
-
-export const updateWorkflow = async (id, updatedData) => {
-  workflows = workflows.map(w =>
-    w._id === id ? { ...w, ...updatedData } : w
-  )
-}
+export const getWorkflows = () => axios.get(API)
+export const createWorkflow = (data) => axios.post(API, data)
+export const deleteWorkflow = (id) => axios.delete(`${API}/${id}`)
+export const updateWorkflow = (id, data) => axios.put(`${API}/${id}`, data) 
